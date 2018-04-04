47f5a8411d8812114a0612dff96b430331005902
RobinExtrant <robin.extrant@gmail.com>
2018-04-04 08:28:10 +0200

    add junit

--- src/test/java/Dataframe_constructor_test.java
+++ src/test/java/Dataframe_constructor_test.java
@@ -4,11 +4,11 @@ import static org.junit.Assert.*;
 import org.junit.*;
 import main.java.Dataframe;
 
-public class test_test {
+public class Dataframe_constructor_test {
 
 	@Test
 	public void test() {
-		Dataframe.Types[] types = {Dataframe.Types.INT, Dataframe.Types.STRING};
+		Object[][] contenu = new Object[][]{{4,5,8},{"Hello", "Goodbye", "Thank you"}};
 		Dataframe datatest = new Dataframe(types);
 		assertEquals(3,3);
 	}
