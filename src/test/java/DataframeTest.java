c7059df2a453c762f835408d69ce9bdaddf41a74
Emmanuel Lionet <emmanuel-lionet@hotmail.fr>
2018-04-13 10:46:42 +0200

    Merge

--- src/test/java/DataframeTest.java
+++ src/test/java/DataframeTest.java
@@ -7,6 +7,7 @@ import main.java.exception.*;
 
 public class DataframeTest {
 
+	//For constructors
 	@Test
 	public void test_Constructor_good() throws TooMuchTypeInOneColumnException, UnknownTypeException, UnequalArraySizeException {
 		Object[][] contenu = new Object[][]{{4,5,8},{"Hello", "Goodbye", "Thank you"},{true, false, true},{4.6, 3.8, 2.6}};
@@ -38,43 +39,93 @@ public class DataframeTest {
 		Dataframe datatest = new Dataframe(contenu, labels);
 	}
 	
-	@Test
-	public void test_Display_all() throws TooMuchTypeInOneColumnException, UnknownTypeException, UnequalArraySizeException {
+	private Dataframe createGoodDataset() throws TooMuchTypeInOneColumnException, UnknownTypeException, UnequalArraySizeException {
 		Object[][] contenu = new Object[][]{{40,50,60},{"Hello", "Goodbye", "Thank you"}};
 		String [] labels = new String[]{"entiers", "string"};
 		Dataframe datatest = new Dataframe(contenu, labels);
+		return datatest;
+	}
+	
+	//For display all
+	@Test
+	public void test_Display_all() throws TooMuchTypeInOneColumnException, UnknownTypeException, UnequalArraySizeException {
+		Dataframe datatest = createGoodDataset();
 		datatest.displayAll();
 	}
 	
+	//For display first line
 	@Test
 	public void test_Display_first_lines_good() throws TooMuchTypeInOneColumnException, UnknownTypeException, UnequalArraySizeException {
-		Object[][] contenu = new Object[][]{{40,50,46},{"Hello", "Goodbye", "Thank you"}};
-		String [] labels = new String[]{"entiers", "string"};
-		Dataframe datatest = new Dataframe(contenu, labels);
+		Dataframe datatest = createGoodDataset();
 		datatest.displayFirstLines(2);
 	}
 	
 	@Test(expected = IndexOutOfBoundsException.class)
-	public void test_Display_first_lines_wrong() throws TooMuchTypeInOneColumnException, UnknownTypeException, UnequalArraySizeException {
-		Object[][] contenu = new Object[][]{{40,50,46},{"Hello", "Goodbye", "Thank you"}};
-		String [] labels = new String[]{"entiers", "string"};
-		Dataframe datatest = new Dataframe(contenu, labels);
+	public void test_Display_first_lines_wrong_with_index_too_big() throws TooMuchTypeInOneColumnException, UnknownTypeException, UnequalArraySizeException {
+		Dataframe datatest = createGoodDataset();
 		datatest.displayFirstLines(4);
 	}
+	
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void test_Display_first_lines_wrong_with_negative_index() throws TooMuchTypeInOneColumnException, UnknownTypeException, UnequalArraySizeException {
+		Dataframe datatest = createGoodDataset();
+		datatest.displayFirstLines(-3);
+	}
 
+	//For display last line
 	@Test
 	public void test_Display_last_lines_good() throws TooMuchTypeInOneColumnException, UnknownTypeException, UnequalArraySizeException {
-		Object[][] contenu = new Object[][]{{40,50,46},{"Hello", "Goodbye", "Thank you"}};
-		String [] labels = new String[]{"entiers", "string"};
-		Dataframe datatest = new Dataframe(contenu, labels);
+		Dataframe datatest = createGoodDataset();
 		datatest.displayLastLines(2);
 	}
 	
 	@Test(expected = IndexOutOfBoundsException.class)
-	public void test_Display_last_lines_wrong() throws TooMuchTypeInOneColumnException, UnknownTypeException, UnequalArraySizeException {
-		Object[][] contenu = new Object[][]{{40,50,46},{"Hello", "Goodbye", "Thank you"}};
-		String [] labels = new String[]{"entiers", "string"};
-		Dataframe datatest = new Dataframe(contenu, labels);
+	public void test_Display_last_lines_wrong_with_index_too_big() throws TooMuchTypeInOneColumnException, UnknownTypeException, UnequalArraySizeException {
+		Dataframe datatest = createGoodDataset();
 		datatest.displayLastLines(4);
+	}
+	
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void test_Display_last_lines_wrong_with_negative_index() throws TooMuchTypeInOneColumnException, UnknownTypeException, UnequalArraySizeException {
+		Dataframe datatest = createGoodDataset();
+		datatest.displayLastLines(-3);
+	}
+	
+	//For select firstline
+	@Test
+	public void test_Select_first_lines_good() throws TooMuchTypeInOneColumnException, UnknownTypeException, UnequalArraySizeException, UnequalColumnSizeException, UncorrectParameterOrderException {
+		Dataframe datatest = createGoodDataset();
+		datatest.selectFirstLines(2);
+	}
+	
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void test_Select_first_lines_wrong_with_index_too_big() throws TooMuchTypeInOneColumnException, UnknownTypeException, UnequalArraySizeException, UnequalColumnSizeException, UncorrectParameterOrderException {
+		Dataframe datatest = createGoodDataset();
+		datatest.selectFirstLines(4);
+	}
+	
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void test_Select_first_lines_wrong_with_negative_index() throws TooMuchTypeInOneColumnException, UnknownTypeException, UnequalArraySizeException, UnequalColumnSizeException, UncorrectParameterOrderException {
+		Dataframe datatest = createGoodDataset();
+		datatest.selectFirstLines(-3);
+	}
+	
+	//For select lastline
+	@Test
+	public void test_Select_last_lines_good() throws TooMuchTypeInOneColumnException, UnknownTypeException, UnequalArraySizeException, UnequalColumnSizeException, UncorrectParameterOrderException {
+		Dataframe datatest = createGoodDataset();
+		datatest.selectLastLines(2);
+	}
+	
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void test_Select_last_lines_wrong_with_index_too_big() throws TooMuchTypeInOneColumnException, UnknownTypeException, UnequalArraySizeException, UnequalColumnSizeException, UncorrectParameterOrderException {
+		Dataframe datatest = createGoodDataset();
+		datatest.selectLastLines(4);
+	}
+	
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void test_Select_last_lines_wrong_with_negative_index() throws TooMuchTypeInOneColumnException, UnknownTypeException, UnequalArraySizeException, UnequalColumnSizeException, UncorrectParameterOrderException {
+		Dataframe datatest = createGoodDataset();
+		datatest.selectLastLines(-3);
 	}
 }
